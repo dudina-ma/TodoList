fbd7ac870bd5d64de39ea65297545ac3331c84cd
Maria Dudina <dudina-ma@mail.ru>
2021-12-15 18:51:08 +0300

    Join check and delete scripts

---
+++ public/scripts/script.js
@@ -0,0 +1,28 @@
+const todos = document.querySelectorAll(".todo");
+
+for (let todo of todos) {
+    todo.addEventListener("change", handlerCheck);
+}
+
+function handlerCheck(event) {
+    const todo = event.currentTarget;
+    fetch("/api/todo/" + todo.dataset.todoId + "/check/" + event.target.checked, {method: 'POST'})
+        .then(() => {
+            todo.classList.toggle('done');;
+        });
+}
+
+const deleteBtns = document.querySelectorAll(".delete-btn");
+
+for (let deleteBtn of deleteBtns) {
+    deleteBtn.addEventListener("click", handlerDelete);
+}
+
+function handlerDelete(event) {
+    const todoId = event.currentTarget.dataset.todoId;
+
+    fetch("/api/todo/" + todoId + "/delete/", {method: 'POST'})
+        .then(() => {
+            window.Modal.showAlert("Delete todo", "Todo has deleted", () => window.location.href = '/');
+        });
+}
