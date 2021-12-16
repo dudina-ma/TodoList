3c2b47213ec44479fff58fcd1dd4c321c5d3d75d
Maria Dudina <dudina-ma@mail.ru>
2021-12-16 19:48:04 +0300

    Add manual form submission (edit todo)

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -29,27 +29,53 @@ function handlerDelete(event) {
         });
 }
 
-// submit form
+// submit createForm
 const createForm = document.querySelector(".create-form");
-createForm.addEventListener("submit", handlerCreate);
+if (createForm) {
+    createForm.addEventListener("submit", handlerCreate);
 
-function handlerCreate(event) {
-    event.preventDefault();
+    function handlerCreate(event) {
+        event.preventDefault();
 
-    let formData = new FormData(event.target);
+        let formData = new FormData(event.target);
 
-    let newTodo = {};
-    formData.forEach((value, key) => newTodo[key] = value);
+        let newTodo = {};
+        formData.forEach((value, key) => newTodo[key] = value);
 
-    fetch("/api/todo/create/",
-        {
+        fetch("/api/todo/create/", {
             method: 'POST',
             body: JSON.stringify(newTodo),
             headers: {
                 'Content-Type': 'application/json',
             },
-        }).
-                then(() => {
-                    window.Modal.showAlert("Create todo", "Todo has been created", () => window.location.href = '/');
-                });
+        }).then(() => {
+             window.Modal.showAlert("Create todo", "Todo has been created", () => window.location.href = '/');
+            });
+    }
+}
+
+const editForm = document.querySelector(".edit-form");
+if (editForm) {
+    editForm.addEventListener("submit", handlerEdit);
+
+    function handlerEdit(event) {
+        event.preventDefault();
+
+        let formData = new FormData(event.target);
+
+        let editedTodo = {};
+        formData.forEach((value, key) => editedTodo[key] = value);
+
+        const todoId = event.target.dataset.todoId;
+
+        fetch("/api/todo/" + todoId + "/edit/", {
+            method: 'POST',
+            body: JSON.stringify(editedTodo),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then(() => {
+             window.Modal.showAlert("Create todo", "Todo has been edited", () => window.location.href = '/');
+        });
+    }
 }
