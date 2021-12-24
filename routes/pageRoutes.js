455ca755a233228d604708f86377241189798486
Maria Dudina <dudina-ma@mail.ru>
2021-12-24 15:27:02 +0300

    Start adding categories

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -1,4 +1,5 @@
 ﻿const { todos } = require('../store');
+const { categories } = require('../store');
 
 const pageRoutes = [
 	{
@@ -7,13 +8,22 @@ const pageRoutes = [
 		getData(params, queryParams) {
 			let currentPage = queryParams.page ?? 1;
 
-			let results = [...todos];
+			let results = todos;
 
 			if (queryParams.search) {
 				const searchStringLowerCase = queryParams.search.toLowerCase();
 				results = todos.filter(item => item.title.toLowerCase().includes(searchStringLowerCase)
 					|| item.description.toLowerCase().includes(searchStringLowerCase));
 			}
+
+			results = todos.map(item => ({
+				id: item.id,
+				title: item.title,
+				isDone: item.isDone,
+				category: categories.find(i => i.id === item.category).title,
+				isUrgent: item.isUrgent,
+				description: item.description
+			}));
 
 			return {
 				todos: results,
@@ -38,6 +48,27 @@ const pageRoutes = [
 		page: 'edit.eta',
 		getData(params) {
 			return todos.find(t => t.id === Number(params.id));
+		}
+	},
+	{
+		url: '/categories/',
+		page: 'categories.eta',
+		getData(params, queryParams) {
+			let currentPage = queryParams.page ?? 1;
+
+			let results = categories;
+
+			if (queryParams.search) {
+				const searchStringLowerCase = queryParams.search.toLowerCase();
+				results = todos.filter(item => item.title.toLowerCase().includes(searchStringLowerCase)
+					|| item.description.toLowerCase().includes(searchStringLowerCase));
+			}
+
+			return {
+				categories: results,
+				currentPage,
+				searchString: queryParams.search,
+			};
 		}
 	}
 ];
--- store.js
+++ store.js
@@ -2,7 +2,7 @@
 	id: 1,
 	title: 'Shop',
 	isDone: false,
-	category: 'private',
+	category: 2,
 	isUrgent: false,
 	description: 'To buy eggs'
 },
@@ -10,7 +10,7 @@
 	id: 2,
 	title: 'Wash clothes',
 	isDone: false,
-	category: 'private',
+	category: 1,
 	isUrgent: false,
 	description: 'White clothes'
 },
@@ -18,7 +18,7 @@
 	id: 3,
 	title: 'Wash the dishes',
 	isDone: true,
-	category: 'private',
+	category: 1,
 	isUrgent: true,
 	description: 'With soap'
 },
@@ -26,7 +26,7 @@
 	id: 4,
 	title: 'Visit a doctor',
 	isDone: false,
-	category: 'private',
+	category: 5,
 	isUrgent: false,
 	description: '12.24.2021'
 },
@@ -34,7 +34,7 @@
 	id: 5,
 	title: 'Call to mom',
 	isDone: false,
-	category: 'private',
+	category: 3,
 	isUrgent: true,
 	description: 'Tell about a new student program'
 },
@@ -42,7 +42,7 @@
 	id: 6,
 	title: 'Return the book to Tom',
 	isDone: false,
-	category: 'private',
+	category: 4,
 	isUrgent: true,
 	description: 'Twilight'
 },
@@ -50,7 +50,7 @@
 	id: 7,
 	title: 'Feed the cats',
 	isDone: false,
-	category: 'private',
+	category: 1,
 	isUrgent: false,
 	description: 'tuesday, thursday, saturday, sunday'
 },
@@ -58,10 +58,32 @@
 	id: 8,
 	title: 'Watch the film',
 	isDone: false,
-	category: 'private',
+	category: 1,
 	isUrgent: false,
 	description: 'Nobody'
 },
 ];
 
-module.exports = { todos };
+const categories = [{
+	id: 1,
+	title: 'home',
+},
+{
+	id: 2,
+	title: 'shop',
+},
+{
+	id: 3,
+	title: 'family',
+},
+{
+	id: 4,
+	title: 'friends',
+},
+{
+	id: 5,
+	title: 'health',
+},
+];
+
+module.exports = { todos, categories };
